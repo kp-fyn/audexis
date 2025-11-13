09d37e4962fa59accfe720adbd13818063ab65df
kp-fyn <kp.fyban@gmail.com>
2025-11-12 21:16:26 -0500

    flash still there

--- apps/www/public/noFlash.js
+++ apps/www/public/noFlash.js
@@ -4,6 +4,10 @@ if (
   (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
   document.documentElement.setAttribute("data-theme", "dark");
+  document.documentElement.style.backgroundColor = "#0a0a0a";
+  document.body.style.backgroundColor = "#0a0a0a";
 } else {
   document.documentElement.setAttribute("data-theme", "light");
+  document.documentElement.style.backgroundColor = "#f1f1f1";
+  document.body.style.backgroundColor = "#f1f1f1";
 }
